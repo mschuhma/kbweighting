e366ffc154f13ed039327800f31f06cacd3ef489
mschuhma <mschuhma@users.noreply.github.com>
2015-06-09 12:23:27 +0200

    igraph mpi cleaned

--- igraph_sp_mpi.R
+++ igraph_sp_mpi.R
@@ -1,8 +1,3 @@
-# This example comes from an old version of the Wikipedia article on
-# bootstrapping, which actually included R code.  The task being executed
-# in the foreach loop executes very quickly, but it does a lot of trials.
-# That makes it a good example of the need for the chunkSize option.
-
 ###### Setting up libs ##########
 #Function to check whether package is installed
   is.installed <- function(mypkg){
@@ -12,18 +7,16 @@ dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
 
 library(doMPI)
 if (!is.installed("igraph")){
-  install.packages("igraph", dependencies=TRUE, Sys.getenv("R_LIBS_USER"), repos = "http://cran.case.edu")
+  install.packages("igraph", dependencies=TRUE, Sys.getenv("R_LIBS_USER"), repos = "http://cran.uni-muenster.de/")
 }
 library(igraph)
 if (!is.installed("plyr")){
-  install.packages("plyr", dependencies=TRUE, Sys.getenv("R_LIBS_USER"), repos = "http://cran.case.edu")
+  install.packages("plyr", dependencies=TRUE, Sys.getenv("R_LIBS_USER"), repos = "http://cran.uni-muenster.de/")
 }
 library(plyr)
 
-###### #Loading data ########
-graph_file <- "data/eta-dataset-joern-hess.data.txt.dbpgraph.sample"
+####### Define functions #######
 
-#weighting functions
 idf <- function(edge_id, edge_cnts, edge_freq_sum) {
   frq <- cnt$freq[cnt$x[edge_id]]
   idf <- log(n/frq)
@@ -40,50 +33,49 @@ inv_x <- function(x, max_x) {
 }
 
 
-####### #Load graph
+####### Load graph #######
 message("loading data")
+graph_file <- "data/eta-dataset-joern-hess.data.txt.dbpgraph"
 input <- read.csv(graph_file, header=F, sep = "", stringsAsFactors=F)
 g <- graph.data.frame(input[,1:2], directed=TRUE)
 weights <- input[,3:3]
 
+####### Create MPI cluster ######
+message("register MPI cluster")
+cl <- startMPIcluster()
+registerDoMPI(cl)
+message(sprintf('Parallel time using doMPI on %d workers', getDoParWorkers()))
+
+####### Start real computation #######
 
 #Weighting edges
 message("computing edge weights")
 cnt <- count(weights)
 n <- sum(cnt$freq)
-weights_idf <- sapply(weights, idf, c(cnt, n))
+weights_idf <- foreach (x=weights, .combine='c') %dopar% {idf(x, cnt, n)}
+#weights_idf <- sapply(weights, idf, c(cnt, n))
 weights_idf <- norm_weights(weights_idf)
 write.csv(weights_idf, file="data/edge_weights_idf.data")
 E(g)$weight <- weights_idf
 message("edge weights written to data/edge_weights_idf.data")
 
-
-###########################################################
-# Create and register an MPI cluster
-#message("register MPI cluster")
-#cl <- startMPIcluster()
-#registerDoMPI(cl)
-message(sprintf('Parallel time using doMPI on %d workers', getDoParWorkers()))
-
-
-######## Shortest path ##########
-
-#foreach (i in seq(1, length(V(g))-1, 1)) {
-#  foreach (j in seq(i+1, length(V(g)), 1)) %dopar% {
-
-rs <- foreach (i=1:length(V(g))-1, .inorder=FALSE, .packages="igraph") %dopar% {
+#Shortest path
+rs <- foreach (i=1:length(V(g))-1, .inorder=FALSE, .packages="igraph") %:% 
        foreach (j=i+1:length(V(g)), .inorder=FALSE, .packages="igraph") %dopar% {
          n1 <- V(g)[i]
          n2 <- V(g)[j]
          if (length(n1$name)*length(n2$name) > 0) {
            val <- shortest.paths(g, v=n1, to=n2, mode="all");
            r <- c(n1, n2, n1$name, n2$name, val)
-           write(r, file="data/all_shortest_paths.data", append=TRUE, ncolumns=5)
+           r
+           #write(r, file="data/all_shortest_paths.data", append=TRUE, ncolumns=5)
          }
-      }
   }
+write(rs, file="data/all_shortest_paths.data", append=FALSE, ncolumns=5)
 
 message("done")
+
+####### Cleanup #######
 # Shutdown the cluster and quit
-#closeCluster(cl)
-#mpi.quit()
+closeCluster(cl)
+mpi.quit()
